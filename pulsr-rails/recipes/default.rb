9a3d830a3182fb908e939d70b9ba93a9ba771d2d
Pavel Pachkovsky <pavel.pachkovskij@gmail.com>
2016-03-18 18:41:45 +0300

    switch to opsworks deploy

--- pulsr-rails/recipes/default.rb
+++ pulsr-rails/recipes/default.rb
@@ -13,8 +13,21 @@ node[:deploy].each do |application, deploy|
     path deploy[:deploy_to]
   end
 
-  pulsr_deploy do
+  opsworks_deploy do
     deploy_data deploy
     app application
+  end
+
+  before_migrate do
+    node[:deploy].each do |application, deploy|
+      Chef::Log.info('Creating .env')
+      file "#{release_path}/.env" do
+        owner deploy[:user]
+        group deploy[:group]
+        mode '0660'
+        content OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables]).map{|key, value| "#{key}=#{value}"}.join("\n")
+        action :create
+      end
+    end
   end
 end
